eb4fc5fbee4d92ffc7e69c531c5d88c4e8329c12
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2023-06-22 10:52:28 +0900

    [unrated] Title: 문자열 섞기, Time: 11.87 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,12 +1,10 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        char[] strArr1 = str1.toCharArray();
-        char[] strArr2 = str2.toCharArray();
         
         for(int i = 0; i< str1.length(); i++){
-            answer += Character.toString(strArr1[i]);
-            answer += Character.toString(strArr2[i]);
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
         }
         return answer;
     }
