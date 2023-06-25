75db8c2014de24e136ce71a5ffff1de68947934d
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2023-06-25 23:18:02 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.36 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        int mid = array.length / 2;
+        answer = array[mid];
+        
+        return answer;
+    }
+}
