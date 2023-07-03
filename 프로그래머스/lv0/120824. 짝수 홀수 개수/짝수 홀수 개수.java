6c5e3db5b5a82681e01a170599ca271dfa823718
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2023-07-03 16:10:21 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 2.24 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] numList) {
-        int[] answer = {0, 0};
+        /*int[] answer = {0, 0};
         for(int i = 0; i < numList.length; i++){
             if(numList[i] % 2 == 0){
         		answer[0]++;        
             }else answer[1]++;
         }
         
-        return answer;
+        return answer;*/
+        return IntStream.of((int)Arrays.stream(numList).filter(i -> i%2 ==0).count(), (int)Arrays.stream(numList).filter(i -> i%2 == 1).count()).toArray();
     }
 }
