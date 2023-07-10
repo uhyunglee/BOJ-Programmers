38246810761b38ad3ddeeeb869a86f8681a991a4
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2023-07-10 15:03:00 +0900

    [unrated] Title: 배열 만들기 4, Time: 6.42 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 class Solution {
-    public ArrayList<Integer> solution(int[] arr) {
-        ArrayList<Integer> stk = new ArrayList<>();
+    public int[] solution(int[] arr) {
+        /*ArrayList<Integer> stk = new ArrayList<>();
         int i = 0;
         while(i < arr.length){
             if(stk.size() == 0){
@@ -17,6 +17,27 @@ class Solution {
                 }
             }
         }
-        return stk;
+        int[] answer = new int[stk.size()];
+        for(int j=0; j<stk.size(); j++){
+            answer[j] = stk.get(j);
+        }
+        return answer;*/
+        
+        Stack<Integer> stack = new Stack();
+        for(int i : arr){
+            if(stack.isEmpty()){
+                stack.push(i);
+                continue;
+            }
+            while(!stack.isEmpty() && i <= stack.peek()){
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        int[] answer = new int[stack.size()];
+        for(int i = 0; i<answer.length;i++){
+            answer[answer.length - 1 - i] = stack.pop();
+        }
+        return answer;
     }
 }
