9582728877a08d4661eb22481af63ca131328bcc
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2023-07-18 17:12:17 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.25 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -4,16 +4,16 @@ class Solution {
     public int[] solution(int[] emergency) {
         ArrayList<Integer> list = new ArrayList<>();
         int[] answer = new int[emergency.length];
-        
+
         for(int i : emergency){
             list.add(i);
         }
         Collections.sort(list, Collections.reverseOrder());
-        
+
         for(int j=0; j<emergency.length; j++){
             answer[j] = list.indexOf(emergency[j]) + 1;
         }
-        
+
         return answer;
     }
 }
