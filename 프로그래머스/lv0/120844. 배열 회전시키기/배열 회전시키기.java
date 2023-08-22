d089c682e5a995cb4a2e87da9987f417b7043c92
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2023-08-22 16:25:58 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,19 +1,21 @@
 class Solution {
     public int[] solution(int[] numbers, String direction) {
-        if (direction.equals("right")) {
-            int temp = numbers[numbers.length - 1];
-            for (int i = numbers.length - 2; i >= 0; i--) {
-                numbers[i + 1] = numbers[i];
+        int[] answer = new int[numbers.length];
+        int temp = 0;
+        
+        if(direction.equals("right")){
+            temp = numbers[numbers.length - 1];
+            for(int i = 0; i< numbers.length; i++){
+                answer[(i+1) % numbers.length] = numbers[i % numbers.length];
             }
-            numbers[0] = temp;
-            return numbers;
-        } else {
-            int temp = numbers[0];
-            for (int i = 0; i <= numbers.length - 2; i++) {
-                numbers[i] = numbers[i + 1];
+            answer[0] = temp;
+        }else if(direction.equals("left")){
+            temp = numbers[0];
+            for(int i = 0; i < numbers.length; i++){
+                answer[i % numbers.length] = numbers[(i+1) % numbers.length];
             }
-            numbers[numbers.length - 1] = temp;
-            return numbers;
+            answer[numbers.length - 1] = temp;
         }
+        return answer;
     }
 }
