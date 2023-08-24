6be92097dabf0bab58ddef048e930ec8c76836cd
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2023-08-24 15:26:23 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for(int i = 0; i < my_string.length(); i++){
+            if(my_string.charAt(i) >= 65 && my_string.charAt(i) <= 90){
+                answer[my_string.charAt(i) - 65]++;
+            }else{
+                answer[my_string.charAt(i) - 71]++;
+            }
+        }
+        return answer;
+    }
+}
