2dace0b1eaba9f831253b18237c25cd37f7794d7
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-01-17 17:55:02 +0900

    [Silver III] Title: 개미, Time: 116 ms, Memory: 12904 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -10,26 +10,12 @@ public class Main {
         int y = sc.nextInt();
         int t = sc.nextInt();
 
-        int tx = t % (2 * w);
-        int ty = t % (2 * h);
-        int dx = 1;
-        int dy = 1;
-        while(tx-- > 0){
-            if(x == w){
-                dx = -1;
-            }else if (x == 0){
-                dx = 1;
-            }
-            x += dx;
-        }
+        int currentX = (t + x) % (2 * w);
+        int currentY = (t + y) % (2 * h);
 
-        while(ty-- > 0){
-            if(y == h)
-                dy = -1;
-            else if(y == 0)
-                dy = 1;
-            y += dy;
-        }
-        System.out.println(x + " " + y);
+        if(currentX > w) currentX = 2 * w - currentX;
+        if(currentY > h) currentY = 2 * h - currentY;
+
+        System.out.println(currentX + " " + currentY);
     }
 }
