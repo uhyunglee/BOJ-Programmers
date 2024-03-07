75798a1f9a95f16e05e1d491c0bf82134041f663
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-03-07 16:40:49 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        String orderStr = Integer.toString(order);
+        String str = Integer.toString(order);
         
-        for(int i = 0; i < orderStr.length(); i++){
-            if(orderStr.charAt(i) == '3' || orderStr.charAt(i) == '6' || 
-              orderStr.charAt(i) == '9') answer++;
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if(c == '3' || c == '6' || c == '9') answer++;
         }
         return answer;
     }
