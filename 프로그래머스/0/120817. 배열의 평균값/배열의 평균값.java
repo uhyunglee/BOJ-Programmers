196e61f1e3b883786e36cf61a4342c92c896577b
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-05-09 12:23:25 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0.;
+        for(int number : numbers){
+            answer += number;
+        }
+        
+        return answer / numbers.length;
+    }
+}
