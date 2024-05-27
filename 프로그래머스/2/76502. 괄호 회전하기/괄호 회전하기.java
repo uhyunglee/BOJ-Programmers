ae48d73566cd4a8707e7ad9e5a5c2356b684f7ae
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-05-27 22:46:52 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.17 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        
+        int length = s.length();
+        s += s;
+        int answer = 0;
+        
+        A:for(int i = 0; i < length; i++){
+            ArrayDeque<Character> stack = new ArrayDeque<>();
+            for(int j = i; j < i + length; j++){
+                char c = s.charAt(j);
+                if(!map.containsKey(c)){
+                    stack.push(c);
+                }else{
+                    if(stack.isEmpty() || !stack.pop().equals(map.get(c)))
+                        continue A;
+                }
+            }
+            if(stack.isEmpty()) answer ++;
+        }
+        return answer;
+    }
+}
