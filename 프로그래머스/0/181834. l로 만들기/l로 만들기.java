4810447da0ee84f6ce1a24505408d76ea4ec288b
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-08-09 01:48:14 +0900

    [level 0] Title: l로 만들기, Time: 17.46 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,16 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        myString = myString.replace('a', 'l');
-        myString = myString.replace('b', 'l');
-        myString = myString.replace('c', 'l');
-        myString = myString.replace('d', 'l');
-        myString = myString.replace('e', 'l');
-        myString = myString.replace('f', 'l');
-        myString = myString.replace('g', 'l');
-        myString = myString.replace('h', 'l');
-        myString = myString.replace('i', 'l');
-        myString = myString.replace('j', 'l');
-        myString = myString.replace('k', 'l');
-        return myString;
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
