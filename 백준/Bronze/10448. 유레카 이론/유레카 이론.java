aeb2a7a033ed1c2383a52ccba0f1b16bdeb64b67
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-09-29 21:51:52 +0900

    [Bronze I] Title: 유레카 이론, Time: 256 ms, Memory: 21240 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -2,30 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static boolean[] isEurekaNumber = new boolean[1001];
+    public static void preprocess(){
+        // 1. K보다 작은 삼각수 구하기
+        int[] triangleNumbers = new int[50];
+        int triangleNumberCount = 0;
+        for(int i = 1; ; i++){
+            int triangleNumber = i * (i + 1) / 2;
+            if(triangleNumber >= 1000) break;
+            triangleNumbers[triangleNumberCount++] = triangleNumber;
+        }
+        // 2. 구해진 삼각수 세개의 합으로 K를 나타낼 수 있는지 확인하기
+        // 두개의 합에 하나 더하기
+        boolean[] isSumOfTriangleNumber = new boolean[1001];
+        for(int i = 0; i < triangleNumberCount; i++)
+            for(int j = 0; j< triangleNumberCount; j++){
+                int sum = triangleNumbers[i] + triangleNumbers[j];
+                if(sum < 1000) isSumOfTriangleNumber[sum] = true;
+            }
+
+        for(int i = 1; i < 1000; i++){
+            if(!isSumOfTriangleNumber[i]) continue;
+            for(int j = 0; j < triangleNumberCount; j++){
+                int sum = i + triangleNumbers[j];
+                if(sum <= 1000) isEurekaNumber[sum] = true;
+            }
+        }
+    }
     public static void main(String[] args) throws IOException {
+        //isEurekaNumber[]
+        preprocess();
+
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        int[] arr = new int[44];
-        for(int i = 0; i < arr.length; i++){
-            int n = i + 1;
-            arr[i] = n * (n+1) / 2;
-        }
-
-        for(int testCase = 0; testCase < T; testCase++){
-            int tcNum = sc.nextInt();
-            boolean isEureka = false;
-            for(int i = 0; i < arr.length; i++){
-                for(int j = 0; j < arr.length; j++){
-                    for(int k = 0; k < arr.length; k++){
-
-                        if((arr[i] + arr[j] + arr[k]) == tcNum)
-                            isEureka = true;
-                    }
-                }
-            }
-            if(isEureka == true)
-                System.out.println("1");
-            else System.out.println("0");
+        while(T-- > 0){
+            int K = sc.nextInt();
+            System.out.println(isEurekaNumber[K] ? "1" : "0");
         }
 
     }
