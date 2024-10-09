3f5affbcd562bece3ea19aa6c8b6f6a22f09fe07
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-10-09 19:51:15 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 38.05 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,18 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        for(int i = 1; i < food.length; i++){
-            int cnt = food[i] / 2;
-            while(cnt-- > 0){
-                answer += i;
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
         }
-        answer += "0";
-        
-        String temp = "";
-        for(int i = answer.length() - 2; i >= 0; i--){
-            temp += answer.charAt(i);
-        }
-        return answer + temp;
+
+        return answer;
     }
 }
