61e37d77a417cc4b525f031f25970e6cff91217f
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-10-10 02:29:26 +0900

    [level 1] Title: 옹알이 (2), Time: 2.92 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,16 +1,6 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] word = {"aya", "ye", "woo", "ma"};
-        String[] repeat = {"ayaaya", "yeye", "woowoo", "mama"};
-        // 반복 옹알 예외처리
-        for(int i = 0; i < babbling.length; i++){
-            for(int j = 0; j < 4; j++){
-                babbling[i] = babbling[i].replace(repeat[j], "X");
-                babbling[i] = babbling[i].replace(word[j], " ");
-            }
-            if(babbling[i].trim().length() == 0) answer++;
-        }
         return answer;
     }
 }
