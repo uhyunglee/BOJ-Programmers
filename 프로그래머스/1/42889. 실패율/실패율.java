9bf978e0a1ac28fef9d723cefaf61ce421f7a96e
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-10-19 21:09:44 +0900

    [level 1] Title: 실패율, Time: 8.86 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,20 +1,20 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
         int[] challenger = new int[N + 2];
+        
         for(int i = 0; i < stages.length; i++){
-            challenger[stages[i]] += 1; // 스테이지 별 도전자
+            challenger[stages[i]]++; // 스테이지 별 도전자 수
         }
         HashMap<Integer, Double> fails = new HashMap<>();
         double total = stages.length;
-        
         for(int i = 1; i <= N; i++){
-            if(challenger[i] == 0){
-                fails.put(i, 0.);
-            }else{
+            if(challenger[i] == 0) fails.put(i, 0.0);
+            else{
                 fails.put(i, challenger[i] / total);
-                total -= challenger[i]; // 다음 스테이지 도전자
+                total -= challenger[i];
             }
         }
         return fails.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
