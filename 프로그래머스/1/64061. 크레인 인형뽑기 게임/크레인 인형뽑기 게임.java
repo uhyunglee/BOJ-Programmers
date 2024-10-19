606672054697c17025afd212ad4fe29e7e3bd7a0
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-10-19 16:07:46 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.94 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,38 +1,34 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        // 스택 배열 생성
+        int result = 0;
         ArrayDeque<Integer>[] stack = new ArrayDeque[board.length];
-        int countDolls = 0;
         
         for(int i = 0; i < board.length; i++){
             stack[i] = new ArrayDeque<>();
         }
         
-        // 스택에 board 값 넣기
-        for(int i = board.length - 1; i >= 0; i--){
-            for(int j = 0; j < board[i].length; j++){
-                if(board[i][j] > 0)
-                    stack[j].push(board[i][j]);
+        // board값 넣기
+        for(int j = 0; j < board.length; j++){
+            for(int i = board.length - 1; i >= 0; i--){
+                if(board[i][j] > 0) stack[j].push(board[i][j]);
             }
         }
         
+        // moves 순회
         ArrayDeque<Integer> slot = new ArrayDeque<>();
-        
-        // moves 순회하면서 stack에서 뽑아 바구니에 넣기 : O(M)
-        for(int i = 0; i < moves.length; i++){
-            if(!stack[moves[i] - 1].isEmpty()){
-                int pick = stack[moves[i] - 1].pop();
+        for(int move : moves){
+            if(!stack[move - 1].isEmpty()){
+                int pick = stack[move - 1].pop();
                 if(!slot.isEmpty() && slot.peek() == pick){
                     slot.pop();
-                    countDolls += 2;
-                }
-                else
-                    slot.push(pick);
+                    result += 2;
+                }else slot.push(pick);
             }
-        }	
+        }
         
-        return countDolls;
+        return result;
     }
 }
