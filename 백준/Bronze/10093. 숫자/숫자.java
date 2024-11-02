4eadaf618a29842d7501eedc0fdd878c48481190
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-11-03 03:18:17 +0900

    [Bronze II] Title: 숫자, Time: 764 ms, Memory: 27752 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.java
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -6,18 +6,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] arr = br.readLine().split(" ");
-        int firstNum = Math.min(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]));
-        int secondNum = Math.max(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]));
+        long firstNum = Math.min(Long.parseLong(arr[0]), Long.parseLong(arr[1]));
+        long secondNum = Math.max(Long.parseLong(arr[0]), Long.parseLong(arr[1]));
 
-        int cnt = firstNum != secondNum ? secondNum - firstNum - 1 : 0;
+        long cnt = firstNum != secondNum ? secondNum - firstNum - 1 : 0;
 
         System.out.println(cnt);
         if(cnt != 0) {
-            int tmp = secondNum - 1;
-            for (int i = firstNum + 1; i < secondNum - 1; i += 1) {
+            for (long i = firstNum + 1; i < secondNum; i += 1) {
                 System.out.print(i + " ");
             }
-            System.out.println(tmp);
         }
     }
 }
