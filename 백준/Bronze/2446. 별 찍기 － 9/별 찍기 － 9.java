1f58b326c677f8c0e8262187aceac5db4e77673c
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-11-03 19:08:02 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 372 ms, Memory: 21028 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = N; i > 0; i--){
+            for(int j = 0; j < N - i; j++){
+                System.out.print(" ");
+            }
+            for(int j = (2*i)-1; j > 0; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i < N; i++){
+            for(int j = N-(i+1); j >0; j--){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < (2*i) + 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
