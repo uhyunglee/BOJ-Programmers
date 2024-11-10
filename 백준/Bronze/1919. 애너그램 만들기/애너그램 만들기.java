4a46ac2b95cc825c49b75f09e9a23151b1fffb5d
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-11-10 17:18:13 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,26 +1,34 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.next();
-        String str2 = scanner.next();
-        char[] arr1 = new char[26];
-        char[] arr2 = new char[26];
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] word1 = br.readLine().toCharArray();
+        char[] word2 = br.readLine().toCharArray();
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
         int count = 0;
 
-        for(int i = 0; i < str1.length(); i++){
-            arr1[str1.charAt(i) - 'a']++;
+        // NOTE: 알파벳 인덱스 배열 생성
+        for(int i = 0; i < word1.length; i++){
+            arr1[word1[i] - 97]++;
         }
-        for(int i = 0; i < str2.length(); i++){
-            arr2[str2.charAt(i) - 'a']++;
+        for(int i = 0; i < word2.length; i++){
+            arr2[word2[i] - 97]++;
         }
+
+        // NOTE: 알파벳 배열 순회
         for(int i = 0; i < arr1.length; i++){
-            if(arr1[i] > arr2[i])
-                count += arr1[i] - arr2[i];
-            else if(arr1[i] < arr2[i])
-                count += arr2[i] - arr1[i];
+            if(arr1[i] != arr2[i]){
+                int max = Math.max(arr1[i], arr2[i]);
+                int min = Math.min(arr1[i], arr2[i]);
+                count += (max - min);
+            }
         }
+
         System.out.println(count);
+
     }
 }
