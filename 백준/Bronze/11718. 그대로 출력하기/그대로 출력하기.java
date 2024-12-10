dacd692a6b86568158cf208b53f6ec492bd1fa9a
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-12-10 11:42:27 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        while((str=br.readLine()) != null){
+            System.out.println(str);
+        }
+        br.close();
+    }
+}
