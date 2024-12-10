429b5767c0e0d18d6ecde1812f51d805316b0055
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-12-10 15:59:09 +0900

    [Bronze V] Title: A+B - 4, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -4,16 +4,14 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
         String str = "";
 
         while((str = br.readLine()) != null){
-            int[] arr = Arrays.stream(str.split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            int A = arr[0];
-            int B = arr[1];
-            System.out.println(A + B);
+            st = new StringTokenizer(str, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a+b);
         }
     }
 }
