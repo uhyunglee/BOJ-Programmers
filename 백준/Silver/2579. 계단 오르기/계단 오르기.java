c6ec0a35c8f9ae39f426afad51060d6435cc559d
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-01-06 00:26:18 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -9,13 +9,14 @@ class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        dp = new Integer[n + 1];
+        // NOTE: dp 배열에 score 최댓값 저장
+        dp = new Integer[n + 1]; // Integer로 선언한 이유 : null로 초기화
         scores = new int[n + 1];
-        long maxScore = 0;
 
         for(int i = 1; i < n + 1; i+=1){
             scores[i] = Integer.parseInt(br.readLine());
         }
+
         dp[0] = scores[0];
         dp[1] = scores[1];
 
@@ -23,6 +24,7 @@ class Main {
 
         System.out.println(step(n));
     }
+    // NOTE: dp 배열 채우기 -> n - 1 을 기준으로 나누어 계산하기
     static int step(int n){
         if(dp[n] == null){
             dp[n] = Math.max(step(n - 2), step(n - 3) + scores[n - 1]) + scores[n];
