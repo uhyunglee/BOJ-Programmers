fdd682e28796f07254bc06025f492c1208b12054
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-01-13 11:56:59 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 136 ms, Memory: 15376 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -7,20 +7,20 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        ArrayDeque<Integer> queue = IntStream.range(0, n).map(i -> i + 1).boxed().collect(Collectors.toCollection(ArrayDeque::new));
 
+        StringBuilder sb = new StringBuilder();
+        List<Integer> list = IntStream.range(0, n).boxed().map(i -> i + 1).collect(Collectors.toList());
+
+        int index = 0;
         sb.append("<");
-        while(queue.size() != 1){
-            for(int i = 0; i < k-1; i++){
-                queue.addLast(queue.poll());
-           }
-            sb.append(queue.poll() + ", ");
+        while(list.size() > 1){
+            index = (index + k - 1) % list.size();
+            sb.append(list.remove(index) + ", ");
         }
-        sb.append(queue.poll() + "> ");
-
+        sb.append(list.remove(0) + ">");
         System.out.println(sb.toString());
+
     }
 }
