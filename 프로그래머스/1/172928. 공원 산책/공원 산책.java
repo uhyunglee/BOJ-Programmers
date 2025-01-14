0a5973f5bfc81b5af9fdb90c5cdacd80534805ee
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-01-14 21:22:57 +0900

    [level 1] Title: 공원 산책, Time: 11.93 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -6,17 +6,14 @@ class Solution {
         StringTokenizer st;
         String[][] map = new String[park.length][park[0].length()];
         int height = map.length;
-        int weight = map[0].length;
+        int width = map[0].length;
         // NOTE: 위치 초기화
         int posX = -1;
         int posY = -1;
-        // NOTE: 주어진 park 배열을 2차원 배열로 전환 
+        
+        // NOTE: 주어진 park 배열을 2차원 배열로 전환 && 시작점 찾기(S)
         for(int i = 0; i < map.length; i++){
             map[i] = park[i].split("");
-        }
-        
-        // NOTE: 시작점 찾기(S)
-        for(int i = 0; i < map.length; i++){
             for(int j = 0; j < map[i].length; j++){
                 if(map[i][j].equals("S")){
                     posX = i;
@@ -24,6 +21,7 @@ class Solution {
                 }
             }
         }
+        
         
         // NOTE: 경로 실행 (장애물 있으면 명령 취소 || 경계밖으로도 나가면 취소)
         for(int i = 0; i < routes.length; i++){
@@ -33,6 +31,7 @@ class Solution {
         	boolean obstacle = false;
             
             if(direction.equals("N")){
+                
                 if(posX - space < 0) continue;
                 
                 for(int j = 1; j <= space; j++){
@@ -42,7 +41,9 @@ class Solution {
                     }
                 }
                 if(!obstacle) posX -= space;
+                
             }else if(direction.equals("S")){
+                
                 if(posX + space >= height) continue;
                 
                 for(int j = 1; j <= space; j++){
@@ -52,7 +53,9 @@ class Solution {
                     }
                 }
                 if(!obstacle) posX += space;
+                
             }else if(direction.equals("W")){
+                
                 if(posY - space < 0) continue;
                 
                 for(int j = 1; j <= space; j++){
@@ -62,8 +65,10 @@ class Solution {
                     }
                 }
                 if(!obstacle) posY -= space;
+                
             }else if(direction.equals("E")){
-                if(posY + space >= weight) continue;
+                
+                if(posY + space >= width) continue;
                 
                 for(int j = 1; j <= space; j++){
                     if(map[posX][posY + j].equals("X")){
@@ -72,12 +77,11 @@ class Solution {
                     }
                 }
                 if(!obstacle) posY += space;
+                
             }
         }
         
-        int[] answer = new int[2];
-        answer[0] = posX;
-        answer[1] = posY;
+        int[] answer = {posX, posY};
         
         return answer;
     }
