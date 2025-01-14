90b5ddf3ebb0b0c86f6ae0acc9ef27a69be91c36
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-01-15 00:35:38 +0900

    [level 1] Title: 바탕화면 정리, Time: 3.50 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,17 +1,40 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int[] answer = {51, 51, -1, -1};
+        int[] answer = new int[4];
+        // NOTE: 바탕화면 2차원 배열 선언
+        int x = wallpaper.length; 
+        int y = wallpaper[0].length();
+        String[][] screen = new String[x][y];
         
-        for(int i = 0; i < wallpaper.length; i++){
-            for(int j = 0; j < wallpaper[0].length(); j++){
-                if(wallpaper[i].charAt(j) == '#'){
-                    answer[0] = Math.min(answer[0], i);
-                    answer[1] = Math.min(answer[1], j);
-                    answer[2] = Math.max(answer[2], i+1);
-                    answer[3] = Math.max(answer[3], j+1);
+        // NOTE: 바탕화면 입력
+        for(int i = 0; i < x; i++){
+            screen[i] = wallpaper[i].split("");
+        }
+        
+        // NOTE: 완전탐색
+        int lux = Integer.MAX_VALUE;
+        int luy = Integer.MAX_VALUE; // 처음 세로, 처음 가로
+        int rdx = Integer.MIN_VALUE;
+        int rdy = Integer.MIN_VALUE; // 마지막 세로, 마지막 가로
+        
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                if(screen[i][j].equals("#")){
+                    if(lux > i) lux = i;
+                    if(luy > j) luy = j;
+                    if(rdx < i) rdx = i;
+                    if(rdy < j) rdy = j;
                 }
             }
         }
+        answer[0] = lux;
+        answer[1] = luy;
+        answer[2] = rdx + 1;
+        answer[3] = rdy + 1;
+        
+        
         return answer;
     }
 }
