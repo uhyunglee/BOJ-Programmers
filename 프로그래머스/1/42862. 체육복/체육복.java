14458cf45d77df662827a006ab03f0e7f5eeec78
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-01-23 01:21:52 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,28 +1,27 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-    	int[] students = new int[n+2];
-        int possible = 0;
+        int answer = 0;
+        int[] students = new int[n+2];
         for(int i : lost){
             students[i]--;
         }
         for(int i : reserve){
             students[i]++;
         }
-        for(int i = 1; i <= n; i++){
-            if(students[i] < 0 && students[i-1] >= 1){
-                students[i] += 1;
-                students[i-1] -= 1;
+        for(int i = 1; i < students.length-1; i++){
+            if(students[i - 1] < 0 && students[i] >= 1){
+                students[i - 1]++;
+                students[i]--;
             }
-            if(students[i] < 0 && students[i+1] >= 1){
-                students[i] += 1;
-                students[i+1] -= 1;
+            if(students[i + 1] < 0 && students[i] >= 1){
+                students[i + 1]++;
+                students[i]--;
             }
         }
+        
         for(int i = 1; i <= n; i++){
-            if(students[i] >= 0) possible++;
+            if(students[i] >= 0) answer++;
         }
-        return possible;
+        return answer;
     }
 }
