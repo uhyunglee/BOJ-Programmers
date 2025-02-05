03528be4a7d43a91dd4d4795bfa2a556eea6ce68
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-02-05 22:25:37 +0900

    [level 1] Title: 대충 만든 자판, Time: 3.21 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,37 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        int[] answer = new int[targets.length];
-        Map<Character, Integer> hashMap = new HashMap<>();
-        for(String str : keymap){
-            StringBuilder sb = new StringBuilder(str);
-            for(int j = 0; j < sb.length(); j++){
-                char c = sb.charAt(j);
-                if(!hashMap.containsKey(c)){
-                    hashMap.put(c, j + 1);
-                }else{
-                    if(hashMap.get(c) > j + 1){
-                        hashMap.put(c, j + 1);
-                    }
-                }
-            }
-        }
-        
-        int i = 0;
-        for(String str : targets){
-            StringBuilder sb = new StringBuilder(str);
-            int count = 0;
-            int value = 0;
-            for(int j = 0; j < sb.length(); j++){
-                char c = sb.charAt(j);
-                value = hashMap.getOrDefault(c, 0);
-                if(value == 0) break;
-                count += value;
-            }
-            
-            answer[i++] = value == 0 ? -1 : count;
-        }
+        int[] answer = {};
         return answer;
     }
 }
