ce40e0b072419ad6e307044a053c7d2c1cac2390
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-02-11 23:58:20 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 184 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args)throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+
+        if(a > b) System.out.print(">");
+        else if(a < b) System.out.print("<");
+        else System.out.print("==");
+    }
+}
