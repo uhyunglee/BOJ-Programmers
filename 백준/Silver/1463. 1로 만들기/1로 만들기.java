2e32e4805f9528ead04cab8fccdea20d06c8eeab
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-03-13 07:59:04 +0900

    [Silver III] Title: 1로 만들기, Time: 292 ms, Memory: 69684 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Integer[] dp;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        dp = new Integer[n + 1];
+        dp[0] = dp[1] = 0;
+
+        System.out.println(recur(n));
+    }
+
+    static int recur(int n ){
+        if(dp[n] == null){
+            if(n  % 6 == 0){
+                dp[n] = Math.min(recur(n - 1), Math.min(recur(n / 3), recur(n / 2))) + 1;
+            }
+            else if(n % 3 == 0){
+                dp[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
+            }
+            else if(n % 2 == 0){
+                dp[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
+            }
+            else{
+                dp[n] = recur(n - 1) + 1;
+            }
+        }
+        return dp[n];
+    }
+}
