f23a7de419d5b2999d18125a86d3af70cc3c8043
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-03-19 07:54:19 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if(n % 4 == 0 && (n % 100 != 0 || n % 400 == 0)){
+            System.out.println("1");
+        }else System.out.println("0");
+    }
+}
