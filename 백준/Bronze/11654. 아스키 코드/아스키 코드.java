677683dcb47d306f2fc2fe798c58d2bef67adbad
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-03-20 07:27:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(str.charAt(0) - 0);
+    }
+}
