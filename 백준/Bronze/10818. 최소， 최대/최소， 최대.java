1493bb117fdc11724b31e1f6aeb6fe28524c1a79
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-04-05 17:37:45 +0900

    [Bronze III] Title: 최소, 최대, Time: 452 ms, Memory: 88428 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -3,14 +3,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+
+        int n = Integer.parseInt(br.readLine());
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         while(n -- > 0){
-            int num = sc.nextInt();
+            int num = Integer.parseInt(st.nextToken());
             if(num < min){
                 min = num;
             }
