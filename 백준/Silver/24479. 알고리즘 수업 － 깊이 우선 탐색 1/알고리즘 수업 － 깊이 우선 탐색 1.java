497fcceec432fb33f68dd4d2f680e719f8039389
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-04-22 07:51:40 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 940 ms, Memory: 92456 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -6,40 +6,40 @@ public class Main {
     static boolean[] visited;
     static int[] order;
     static int count = 1;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int r = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        order = new int[n + 1];
-        for(int i = 1; i <= n; i++){
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        order = new int[N + 1];
+        for(int i = 1; i <= N; i++){
             graph[i] = new ArrayList<>();
         }
-        for(int i = 0; i < m; i++){
+        for(int i = 1; i <= M; i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             graph[u].add(v);
             graph[v].add(u);
         }
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i <= N; i++){
             Collections.sort(graph[i]);
         }
-        dfs(r);
-        for(int i = 1; i <= n; i++){
+        dfs(R);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 1; i <= N; i++){
             bw.write(order[i] + "\n");
         }
         bw.flush();
         bw.close();
     }
-
-    private static void dfs(int node){
+    static void dfs(int node){
         visited[node] = true;
         order[node] = count++;
 
