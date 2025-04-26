662683c396167a3651c31d8743e38f7060973936
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-04-26 23:57:54 +0900

    [Silver V] Title: 영화감독 숌, Time: 340 ms, Memory: 89460 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while(count != N) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
