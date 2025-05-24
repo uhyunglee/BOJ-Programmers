ca4384675f2ea739b41055b1d216faaa304b0505
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-05-25 00:28:16 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1648 ms, Memory: 301592 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,23 +1,24 @@
+// 접근방법 계수정렬
+// 시간복잡도 O(K) K : 10,001
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
         int[] cnt = new int[10001];
-
-        for(int i = 0; i < N; i++){
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
             cnt[Integer.parseInt(br.readLine())]++;
         }
-        // 출력
         for(int i = 0; i < 10001; i++){
             while(cnt[i]-- > 0){
                 bw.write(i + "\n");
             }
         }
         bw.flush();
+        bw.close();
     }
 }
