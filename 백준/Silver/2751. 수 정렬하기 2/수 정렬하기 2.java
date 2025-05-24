e0d3e8e3534484247be2bf9995273361f8074406
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-05-24 22:40:39 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1448 ms, Memory: 115448 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,3 +1,6 @@
+// 시간복잡도 O(n)
+// 접근방법 n <= 1,000,000 이므로 출력때 BufferedWriter 사용
+
 import java.io.*;
 import java.util.*;
 
@@ -5,14 +8,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         for(int i = 0; i < n; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
-        for(int num : arr){
-            bw.write(num + "\n");
+        for(int i = 0; i < n; i++){
+            bw.write(arr[i] + "\n");
         }
         bw.flush();
         bw.close();
