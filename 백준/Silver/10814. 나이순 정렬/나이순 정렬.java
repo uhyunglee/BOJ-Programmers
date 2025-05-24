fcb6a2e0aa733ede60465e7299db81091de5ed92
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-05-25 01:38:58 +0900

    [Silver V] Title: 나이순 정렬, Time: 376 ms, Memory: 41488 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,3 +1,6 @@
+// 접근방법: 계수정렬 응용 
+// 시간복잡도 O(N) N<= 100,000
+
 import java.io.*;
 import java.util.*;
 
@@ -5,26 +8,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder[] ageGroups = new StringBuilder[201];
 
-        StringBuilder[] person = new StringBuilder[201];
-
-        for(int i = 0; i < person.length; i++) {
-            person[i] = new StringBuilder();
+        for(int i = 0; i < ageGroups.length; i++){
+            ageGroups[i] = new StringBuilder();
         }
-
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            person[age].append(age).append(' ').append(name).append('\n');
+            ageGroups[age].append(age).append(' ').append(name).append('\n');
         }
-
-        StringBuilder sb = new StringBuilder();
-        for(StringBuilder p : person) {
-            sb.append(p);
+        StringBuilder result = new StringBuilder();
+        for(StringBuilder person : ageGroups){
+            result.append(person);
         }
-
-        System.out.println(sb);
+        System.out.println(result);
     }
 }
