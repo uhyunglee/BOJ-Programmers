990bfdb4e1331475579a5c55823e5c7c19c6d057
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-05-25 12:28:48 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 676 ms, Memory: 52100 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,39 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.io.IOException;
- 
+// 접근방법: y좌표 값을 첫번쩨 배열에 입력 후 정렬
+// 시간복잡도 O(NlogN) : Arrays.sort
+
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int[][] arr = new int[N][2];
-		
-		StringTokenizer st;
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr, (e1, e2) -> {
-			if(e1[0] == e2[0]) {
-				return e1[1] - e2[1];
-			} else {
-				return e1[0] - e2[0];
-			}
-		});
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0 ; i< N ; i++) {
-			sb.append(arr[i][1] + " " + arr[i][0]).append('\n');
-		}
-		System.out.println(sb);
-	}
- 
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] grid = new int[n][2];
+
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            grid[i][1] = Integer.parseInt(st.nextToken());
+            grid[i][0] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(grid, (e1, e2) ->{
+            if(e1[0] == e2[0]){
+                return e1[1] - e2[1];
+            }else{
+                return e1[0] - e2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            sb.append(grid[i][1]).append(" ").append(grid[i][0]).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
