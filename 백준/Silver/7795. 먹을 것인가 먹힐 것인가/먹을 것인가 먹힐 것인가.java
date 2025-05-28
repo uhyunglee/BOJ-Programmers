3ecb7ec77e0c6f4d81f451945469fecdb95fbb38
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-05-28 23:59:15 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 472 ms, Memory: 39720 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,47 @@
+// 시간복잡도 : O(nlogn)
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder result = new StringBuilder();
+
+        int tc = Integer.parseInt(br.readLine());
+
+
+        while(tc-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int[] A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] B = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            Arrays.sort(B);
+            int temp = 0;
+            for(int i=0;i<N;i++) {
+                int low = 0;
+                int high = M-1;
+                int cnt = 0;
+                while(low<=high) {
+                    int mid = (low+high)/2; //중간값
+                    if(B[mid]<A[i]) {
+                        low = mid+1;
+                        cnt = mid+1;
+                    }
+                    else
+                        high = mid-1;
+                }
+                temp+=cnt;
+            }
+            result.append(temp+"\n");
+        }
+
+        bw.write(result+"");
+        bw.flush();
+        bw.close();
+        
+    }
+}
