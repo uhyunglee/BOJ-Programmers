c29955a8f8bea249ca17d046a3b5faf247a9fea0
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-06-05 15:19:25 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1424 ms, Memory: 338716 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,4 +1,5 @@
-
+// 시간복잡도: O(N * M * K)
+// 접근방법: 벽 부순 횟수에 따라 분기처리
 import java.io.*;
 import java.util.*;
 
@@ -11,50 +12,63 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        N = arr[0];
-        M = arr[1];
-        K = arr[2];
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        K = Integer.parseInt(input[2]);
+
         map = new int[N][M];
-        visited = new boolean[N][M][K+1];
-        for(int i = 0; i < N; i++){
-            map[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+        visited = new boolean[N][M][K + 1];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
         }
 
         System.out.println(bfs());
     }
-    static int bfs(){
+
+    static int bfs() {
         Queue<Point> queue = new ArrayDeque<>();
-        queue.add(new Point(0,0, 0, 1));
+        queue.add(new Point(0, 0, 0, 1));
+        visited[0][0][0] = true;
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Point cur = queue.poll();
-            if(cur.r == N - 1 && cur.c == M - 1) return cur.dist;
 
-            for(int i = 0; i < 4; i++){
+            if (cur.r == N - 1 && cur.c == M - 1) return cur.dist;
+
+            for (int i = 0; i < 4; i++) {
                 int nr = cur.r + dr[i];
-                int nc = cur.c + dc [i];
-                if(nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
-                if(map[nr][nc] == 0 && !visited[nr][nc][cur.breakCount]){
+                int nc = cur.c + dc[i];
+
+                if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+
+                // 빈 칸
+                if (map[nr][nc] == 0 && !visited[nr][nc][cur.breakCount]) {
                     visited[nr][nc][cur.breakCount] = true;
                     queue.add(new Point(nr, nc, cur.breakCount, cur.dist + 1));
-                }else if(map[nr][nc] == 1 && cur.breakCount < K && !visited[nr][nc][cur.breakCount + 1]){
+                }
+                // 벽이고 아직 부술 수 있음
+                else if (map[nr][nc] == 1 && cur.breakCount < K && !visited[nr][nc][cur.breakCount + 1]) {
                     visited[nr][nc][cur.breakCount + 1] = true;
                     queue.add(new Point(nr, nc, cur.breakCount + 1, cur.dist + 1));
                 }
             }
         }
+
         return -1;
     }
-}
-class Point{
-    int r, c, breakCount, dist;
-    public Point(int r, int c, int breakCount, int dist){
-        this.r = r;
-        this.c = c;
-        this.breakCount = breakCount;
-        this.dist = dist;
+
+    static class Point {
+        int r, c, breakCount, dist;
+        public Point(int r, int c, int breakCount, int dist) {
+            this.r = r;
+            this.c = c;
+            this.breakCount = breakCount;
+            this.dist = dist;
+        }
     }
 }
