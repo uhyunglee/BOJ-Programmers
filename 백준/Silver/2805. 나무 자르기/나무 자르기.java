6ae371086c8b4c3184c5aa1df298d27420c06636
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-06-11 17:02:36 +0900

    [Silver II] Title: 나무 자르기, Time: 492 ms, Memory: 119220 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -2,42 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[] tree = new int[N];
-
-        int min = 0;
+        int[] trees = new int[n];
+        st = new StringTokenizer(br.readLine());
         int max = 0;
-
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++) {
-            tree[i] = Integer.parseInt(st.nextToken());
-            if(max < tree[i]) {
-                max = tree[i];
-            }
+        for(int i = 0; i < n; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, trees[i]);
         }
-        while(min < max) {
 
-            int mid = (min + max) / 2;
+        long left = 0, right = max, answer = 0;
+        while(left <= right) {
+            long mid = (left + right) / 2;
             long sum = 0;
-            for(int treeHeight : tree) {
-                if(treeHeight - mid > 0) {
-                    sum += (treeHeight - mid);
-                }
+            for(int tree : trees) {
+                if(tree > mid) sum += (tree - mid);
             }
-            if(sum < M) {
-                max = mid;
-            }
-            else {
-                min = mid + 1;
+
+            if(sum >= m) {
+                answer = mid; // 가능한 높이 중 최대값을 저장
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        System.out.println(min - 1);
+
+        System.out.println(answer);
     }
 }
