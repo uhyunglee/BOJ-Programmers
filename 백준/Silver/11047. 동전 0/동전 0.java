ea80d498c4f33bc3b85f06fa5887138dffdca4a0
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-07-06 17:12:57 +0900

    [Silver IV] Title: 동전 0, Time: 112 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -4,20 +4,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        int count = 0;
-        int[] wallet = new int[n];
-        for(int i = 0; i < wallet.length; i++){
-            wallet[i] = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[N - 1 - i] = Integer.parseInt(br.readLine());
         }
-        for(int i = wallet.length - 1; i >= 0; i--){
-            while(wallet[i] <= k){
-                k -= wallet[i];
-                count++;
+        int count = 0;
+        while(K > 0){
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] <= K){
+                    count += K / arr[i];
+                    K %= arr[i];
+                }
             }
         }
         System.out.println(count);
