6c15f91be2730f354d3990d3e15de46cfd362920
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2025-07-08 00:20:34 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,25 +1,24 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String expression = br.readLine();
 
+        String expression = br.readLine();
         String[] minusSplit = expression.split("-");
 
         int result = sum(minusSplit[0]);
 
-        for (int i = 1; i < minusSplit.length; i++) {
-            result -= sum(minusSplit[i]); 
+        for(int i = 1; i < minusSplit.length; i++){
+            result -= sum(minusSplit[i]);
         }
-
         System.out.println(result);
     }
-
-    public static int sum(String str) {
+    static int sum(String str){
         String[] tokens = str.split("\\+");
         int sum = 0;
-        for (String token : tokens) {
+        for(String token : tokens){
             sum += Integer.parseInt(token);
         }
         return sum;
